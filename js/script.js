5e44c57669da6920c5a76e77f820cdc6e6bad09b
surface74 <warwhisper@tut.by>
2021-03-20 17:34:42 +0300

    before check position: fixed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
 'use strict';
 
-// const collapseEl = document.querySelector('#navbarNav');
-// collapseEl.addEventListener('show.bs.collapse', e => console.log(e));
+const navbarCollapse = document.querySelector('.navbar-collapse.collapse');
+const navbar = document.querySelector('.navbar.navbar-expand-lg');
+
+const collapseNavbar = event => {
+  const target = event.target;
+  if (target.classList.contains('nav-link') && navbarCollapse.classList.contains('show')) {
+    new window.bootstrap.Collapse(navbarCollapse);
+  }
+};
+
+//992 === .navbar-expand-lg
+const collapseOnWidth = event => {
+  if (navbar.clientWidth >= 992) {
+    new window.bootstrap.Collapse(navbarCollapse);
+  }
+};
+
+navbarCollapse.addEventListener('show.bs.collapse', () => {
+  window.addEventListener('resize', collapseOnWidth);
+});
+
+navbarCollapse.addEventListener('hide.bs.collapse', () => {
+  window.removeEventListener('resize', collapseOnWidth);
+});
+
+navbarCollapse.addEventListener('click', collapseNavbar);
+
